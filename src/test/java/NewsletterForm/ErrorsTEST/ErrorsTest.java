62d4dd3fd48399abc99e36ef31b68f635b6b2b1e
skondratieva <kondratieva.ua@gmail.com>
2019-04-10 14:50:43 +0300

    improvement

--- src/test/java/NewsletterForm/ErrorsTEST/ErrorsTest.java
+++ src/test/java/NewsletterForm/ErrorsTEST/ErrorsTest.java
@@ -4,6 +4,7 @@ import Base.BaseTest;
 import NewsletterForm.Errors;
 import com.codeborne.selenide.Selenide;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ErrorsTest extends BaseTest {
@@ -12,6 +13,11 @@ public class ErrorsTest extends BaseTest {
     Strings strings = new Strings();
     UserInfo userInfo = new UserInfo();
 
+
+    @BeforeMethod
+    public void logOff(){
+        Selenide.clearBrowserCookies();
+        }
 
 
     @Test
